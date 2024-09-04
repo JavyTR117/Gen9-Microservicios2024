2f005adc98077181eb38c9e2d47f3ff13f6b762f
DESKTOP-H81H5U9\TRNET-03 <javier_timal@hotmail.com>
2024-09-04 11:03:27 -0600

    Se agregan los proyecto de NEgocio, Infraestructra y el mircoservicio de Choferes

---
+++ Gen9-Parent2024/Gen9-Negocio2024/Gen9-Choferes2024/src/main/java/com/timal/choferes/Gen9Choferes2024Application.java
@@ -0,0 +1,13 @@
+package com.timal.choferes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gen9Choferes2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gen9Choferes2024Application.class, args);
+	}
+
+}
